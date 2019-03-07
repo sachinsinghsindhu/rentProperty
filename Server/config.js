92b3a4316cb20d71fc0a8788b85d5720657c1128
unknown <sachinsinghsindhu@gmail.com>
2019-03-08 00:39:01 +0530

    mysql test

---
+++ Server/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Port : 3000,
+    saltRounds: 8,
+    connect: {
+        connectionLimit : 10,
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'rentproperty'
+    },
+}
--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const config = require('./config');
+const port = process.env.PORT || config.Port;
+const userQuery = require('./Queries/userQuery');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(cookieParser());
+app.get('/',(req, res, next) => {
+    userQuery.findUser(1,req,res,next);
+},(req, res) => {
+    console.log('next',req.queryRes);
+    res.send(req.queryRes);
+});
+app.listen(port, () => {
+    console.log(`server running at ${port}`);
+});
